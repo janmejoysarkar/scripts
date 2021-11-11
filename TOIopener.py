cb244af4dc3ce13e3d62ecdc39253bce89348517
Janmejoy Sarkar <63224620+janmejoysarkar@users.noreply.github.com>
2021-11-11 07:40:22 +0530

    TOI Opener

--- TOIopener.py
+++ TOIopener.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Opens Latest Delhi Edition TOI in browser.
 New paper available at 7am everyday
@@ -10,7 +12,9 @@ import webbrowser as wb
 url='https://dailyepaper.in/times-of-india-epaper-pdf-download-2021/'
 page= requests.get(url)
 status=page.status_code
-print(status)
+print ('Getting page status')
+if status==200:
+	print('Page is UP')
 soup = BeautifulSoup(page.content, 'html.parser')
 paper= soup.find_all(style ="font-size: 16px;")[0]
 link= paper.find(href=True)['href']
